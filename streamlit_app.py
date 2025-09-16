016020391a025c534b663837fc598c6705cb080e
Okanqk <nokangk00@gmail.com>
2025-09-17 02:54:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1160,9 +1160,8 @@ elif menu == "📝 Testler":
             st.line_chart(daily_df["puan"])
 
             st.subheader("📋 Günlük Detay Tablosu")
-            st.dataframe(daily_df.iloc[::-1])  # Tersten sırala
-        else:
-            st.info("📝 Henüz günlük veri yok.")
+            st.dataframe(daily_df.iloc[::-1])  # Tersten sırala        
+    else:         st.info("📝 Henüz günlük veri yok.")
 
     with tab2:
         st.subheader("📊 Genel İstatistikler")
